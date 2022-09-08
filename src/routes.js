8b115f1223597826bd8fc147b7f5032a05a695f7
EnriqueGS88 <enrique.gzs@gmail.com>
2022-09-08 18:54:56 +0200

    removed layouts

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,4 @@
-import { Navigate, useRoutes } from 'react-router-dom';
-// layouts
-// import DashboardLayout from './layouts/dashboard';
-// import LogoOnlyLayout from './layouts/LogoOnlyLayout';
-// //
-// import Blog from './pages/Blog';
-// import User from './pages/User';
-// import Login from './pages/Login';
-// import NotFound from './pages/Page404';
-// import Register from './pages/Register';
-// import Products from './pages/Products';
+import { useRoutes } from 'react-router-dom';
 import DashboardApp from './pages/DashboardApp';
 
 // ----------------------------------------------------------------------
@@ -18,33 +8,6 @@ export default function Router() {
     {
       path: '/',
       element: <DashboardApp />,
-      // children: [
-      //   { path: 'app', element: <DashboardApp /> },
-      //   { path: 'user', element: <User /> },
-      //   { path: 'products', element: <Products /> },
-      //   { path: 'blog', element: <Blog /> },
-      // ],
     },
-    // {
-    //   path: 'login',
-    //   element: <Login />,
-    // },
-    // {
-    //   path: 'register',
-    //   element: <Register />,
-    // },
-    // {
-    //   path: '/',
-    //   element: <LogoOnlyLayout />,
-    //   children: [
-    //     { path: '/', element: <Navigate to="/dashboard/app" /> },
-    //     { path: '404', element: <NotFound /> },
-    //     { path: '*', element: <Navigate to="/404" /> },
-    //   ],
-    // },
-    // {
-    //   path: '*',
-    //   element: <Navigate to="/404" replace />,
-    // },
   ]);
 }
